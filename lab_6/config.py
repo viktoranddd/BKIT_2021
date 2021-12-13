0b305c1a987fee87071b836d919e67f053ca04f7
viktoranddd <91336238+viktoranddd@users.noreply.github.com>
2021-12-13 23:30:47 +0300

    Add files via upload

---
+++ lab_6/config.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+# Токент бота
+TOKEN = "5094082950:AAHfbYkSZEiZ0QZHiIsnbsRohPIFmXRl3RE"
+
+# Файл базы данных Vedis
+db_file = "db.vdb"
+
+# Ключ записи в БД для текущего состояния
+CURRENT_STATE = "CURRENT_STATE"
+
+# Состояния автомата
+class States(Enum):
+    STATE_START = "STATE_START"  # Начало нового диалога
+    STATE_FIRST_NUM = "STATE_FIRST_NUM"
+    STATE_SECOND_NUM = "STATE_SECOND_NUM"
+    STATE_OPERATION = "STATE_OPERATION"
